a0a27cae4631ad452a67c3cc0e3887323c1b0327
ahmed <ahmedessam.alhussein@gmail.com>
2026-05-03 14:38:38 +0300

    edit-images

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -10,17 +10,17 @@ export class ServicesComponent {
   services = [
     {
       title: 'Advertisers',
-      image: '/assets/media/service-1.jpg',
+      image: 'assets/media/service-1.jpg',
       label: 'Advertisers'
     },
     {
       title: 'Agencies',
-      image: '/assets/media/service-2.jpg',
+      image: 'assets/media/service-2.jpg',
       label: 'Agencies'
     },
     {
       title: 'Media Owners',
-      image: '/assets/media/service-3.jpg',
+      image: 'assets/media/service-3.jpg',
       label: 'Media Owners'
     }
   ];
